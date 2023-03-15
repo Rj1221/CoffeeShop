58c0faae3e5ff13a215e7eb53d2227f5b724d345
Raj <rj6207491172@gmail.com>
2023-03-15 06:46:41 +0530

    About Section Completed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 // Change Background Header
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 // Show Scroll Up
 
-
-
 // Scroll Sections Active Link
-
 
 // Scroll Reveal Animation
